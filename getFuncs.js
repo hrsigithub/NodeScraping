5915d4d6a27853bdb23b94da27d363124ae450d8
Hiroshi Nakai <hrsi.ad@gmail.com>
2024-04-04 10:08:14 +0900

    一旦保存、楽天バグあり

--- getFuncs.js
+++ getFuncs.js
@@ -1,27 +1,45 @@
-
 const axios = require("axios");
-const cheerio = require("cheerio")
+const cheerio = require("cheerio");
 
 function getSP500() {
-    const URL = "https://itf.minkabu.jp/fund/03311187"
-    const data = {};
+  const URL = "https://itf.minkabu.jp/fund/03311187";
+  const data = {};
 
-    axios(URL)
-    .then((res) => { 
-        // console.log(res.data); 
-        const parser = res.data
-        const $ = cheerio.load(parser);
-        $(".fund_price", parser).each(function() {
-            const basePrice = $(this).find(".fund_cv .stock_price").text();
-            const futurePrice = $(this).find(".fd_estimate .stock_price").text();
-    
-            // オブジェクトへ追加
-            data.basePrice = basePrice
-            data.futurePrice = futurePrice
-            console.log(data)
-        })
-    }).catch(err => console.log(err)) 
+  axios(URL)
+    .then((res) => {
+      // console.log(res.data);
+      const parser = res.data;
+      const $ = cheerio.load(parser);
+      $(".fund_price", parser).each(function () {
+        const basePrice = $(this).find(".fund_cv .stock_price").text();
+        const futurePrice = $(this).find(".fd_estimate .stock_price").text();
+
+        // オブジェクトへ追加
+        data.basePrice = basePrice;
+        data.futurePrice = futurePrice;
+        console.log(data);
+      });
+    })
+    .catch((err) => console.log(err));
 }
- 
 
-module.exports = getSP500
+function getRakuten() {
+  const URL = "https://search.rakuten.co.jp/search/mall/keyboard/";
+  const data = [];
+
+  axios(URL)
+    .then((response) => {
+      const htmlParser = response.data;
+      const $ = cheerio.load(htmlParser);
+
+      $(".searchresultitem", htmlParser).each(function () {
+        const title = $(this).find(".title").text();
+        const price = $(this).find(".important").text();
+        data.push({ title, price });
+        console.log(price);
+      });
+    })
+    .catch((error) => console.log(error));
+}
+
+module.exports = { getSP500, getRakuten };
--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const PORT = 3001
+const PORT = 3001;
 
 const exspress = require("express");
-const getSP500 = require("./getFuncs")
-
-// import { getSP500 } from "./getFuncs"
+const { getSP500, getRakuten } = require("./getFuncs");
 
 const app = exspress();
 //app.listen(PORT, console.log("動いてる？"))
 
-getSP500();
-
-
-
-
+// getSP500();
+getRakuten();
